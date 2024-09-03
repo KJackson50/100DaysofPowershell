ddec97ec42fcce4c082af7a86adf9972bb9436a5
KJackson50 <KJackson5053@gmail.com>
2024-09-03 04:55:07 -0400

    Add files via upload

---
+++ Say_Hello_Day12.ps1
@@ -0,0 +1,13 @@
+﻿#function that says 'hello' to whoever you input
+
+function Say-Hello {
+
+$name = Read-Host -Prompt "Enter your name"
+
+write-host "Hello $name!"
+
+}
+
+
+#calling function
+Say-Hello
